227b5f2210ce3d7c559bd927a8c78f60d1822923
Beno Dwianto <benodwianto9@gmail.com>
2024-07-28 15:52:31 +0700

    menambahkan update investasi, memperbarui data index dari database,dan form pengunjung

--- admin/visi_misi.php
+++ admin/visi_misi.php
@@ -3,8 +3,8 @@ include '../config/functions.php'; // Meng-include file fungsi
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id = $_POST['id'];
-    $mengapa_ghaffar = $_POST['mengapa_ghaffar'];
-    $kelebihan = $_POST['kelebihan'];
+    $visi = $_POST['visi'];
+    $misi = $_POST['misi'];
 
     // Periksa apakah ada file yang di-upload
     if (isset($_FILES['foto']) && $_FILES['foto']['error'] === UPLOAD_ERR_OK) {
@@ -14,10 +14,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     // Proses update data
-    $resultMessage = updateLayanan($id, $kelebihan, $mengapa_ghaffar, $fotoFileInputName);
+    $resultMessage = updateVisiMisi($id, $visi, $misi, $fotoFileInputName);
 }
 
-$sql = "SELECT id, kelebihan, mengapa_ghaffar, foto FROM layanan";
+$sql = "SELECT id, visi, misi, foto FROM visi_misi";
 $result = $conn->query($sql);
 ?>
 
@@ -26,30 +26,32 @@ $result = $conn->query($sql);
 
 <head>
     <meta charset="UTF-8">
-    <title>Update Layanan</title>
+    <title>Update tentang</title>
 </head>
 
 <body>
-    <h2>Update Layanan</h2>
+    <h2>Update tentang</h2>
 
     <!-- Menampilkan pesan hasil operasi -->
     <?php if (isset($resultMessage)) : ?>
         <p><?= htmlspecialchars($resultMessage); ?></p>
     <?php endif; ?>
 
-    <!-- Menampilkan formulir input untuk setiap entri layanan -->
+    <!-- Menampilkan formulir input untuk setiap entri tentang -->
     <?php if ($result->num_rows > 0) : ?>
         <?php while ($row = $result->fetch_assoc()) : ?>
-            <form action="layanan.php" method="post" enctype="multipart/form-data">
+            <form action="" method="post" enctype="multipart/form-data">
                 <input type="hidden" name="id" value="<?= htmlspecialchars($row['id']); ?>">
-                <label for="kelebihan">kelebihan:</label>
-                <input type="text" name="kelebihan" id="kelebihan" value="<?= htmlspecialchars($row['kelebihan']); ?>"><br>
-                <label for="mengapa_ghaffar">mengapa_ghaffar:</label>
-                <input type="text" name="mengapa_ghaffar" id="mengapa_ghaffar" value="<?= htmlspecialchars($row['mengapa_ghaffar']); ?>"><br>
+                <label for="visi">visi:</label>
+                <input type="text" name="visi" id="visi" value="<?= htmlspecialchars($row['visi']); ?>"><br>
+
+                <label for="misi">misi:</label>
+                <input type="text" name="misi" id="misi" value="<?= htmlspecialchars($row['misi']); ?>"><br>
+
                 <label for="foto">Foto:</label>
-                <input type="file" name="foto" id="foto"> <img src="../assets/images/layanan/<?= htmlspecialchars(basename($row['foto'])); ?>" alt="foto layanan" width="50" height="50">
+                <input type="file" name="foto" id="foto"> <img src="../assets/images/visi_misi/<?= htmlspecialchars(basename($row['foto'])); ?>" alt="foto tentang" width="50" height="50">
                 <br>
-                <input type="submit" value="Update Layanan">
+                <input type="submit" value="Update visi_misi">
             </form>
             <hr>
         <?php endwhile; ?>
