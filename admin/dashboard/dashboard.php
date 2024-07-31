8f88beba3891d6ef03fc6d466a97c2c47b9bcf5c
rasyidfirdaus482 <rasyidfirdaus53@gmail.com>
2024-07-31 11:59:06 +0700

    halaman read produk dan include config disatu halaman dashboard saja

--- admin/dashboard/dashboard.php
+++ admin/dashboard/dashboard.php
@@ -1,3 +1,4 @@
+<?php include '../../config/functions.php';?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,6 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css"
         rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body>
@@ -24,7 +26,7 @@
                     ob_start(); // Mulai output buffering
                     include 'HalamanDashboard.php';
                     include 'HalamanTentang.php';
-                    include 'HalamanProduk.php';
+                    include '../produk/HalamanProduk.php'; // Pastikan ini sudah benar
                     include 'HalamanLayanan.php';
                     include 'HalamanLegalitas.php';
                     include '../user/HalamanTambahAdmin.php';
